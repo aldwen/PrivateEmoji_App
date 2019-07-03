78b29a977449436204257c86acc07f1688c7aef1
温 温 <santer@live.cn>
2019-07-03 21:03:46 +0800

    提交一下

--- favemoji/urls.py
+++ favemoji/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = 'favemoji' #app name 用于地址引用，区分不同APP 
+app_name = 'favemoji'  # app name 用于地址引用，区分不同APP
 urlpatterns = [
     path('', views.index, name="index"),
+    path('detail/', views.EmojiInfoDetailView.as_view(), name='detail')
 ]
--- favemoji/views.py
+++ favemoji/views.py
@@ -1,5 +1,5 @@
 import time
-# from django.views import generic
+from django.views import generic
 from django.shortcuts import render
 from .models import EmojiInfo
 
@@ -11,10 +11,19 @@ from .models import EmojiInfo
 
 
 def index(request):
-    # 只有一个字符串的首页，第一版out
+    # 只有一个字符串的首页，第一版 is out
     # response=HttpResponse('FavEmoji 的首页')
     # return response
     allemojis = EmojiInfo.objects.all()
     ctime = time.strftime("%Y{y}%m{m}%d{d}%H{h}%M{m1}%S{s}").format(y='年', m='月', d='日', h="时", m1="分", s="秒")
     context = {'emojilist': allemojis, 'current_time': ctime}
     return render(request, 'favemoji/2col.html', context)
+
+
+class EmojiInfoDetailView(generic.ListView):
+    template_name = "favemoji/detail_EmojiInfo.html"
+    context_object_name = 'all_list'
+
+    def get_queryset(self):
+        return EmojiInfo.objects.all()
+    
